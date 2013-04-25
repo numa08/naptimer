dac316c7dd2d4d9fce744d8114a2509c8309bfc3
numa08 <n511287@gmail.com>
2013-04-26 00:30:34 +0900

    ダイアログ実装
    レイアウトとかテーマとかを修正

--- src/naptimer/src/net/numa08/naptimer/widget/RingConfigDialog.java
+++ src/naptimer/src/net/numa08/naptimer/widget/RingConfigDialog.java
@@ -1,5 +1,7 @@
 package net.numa08.naptimer.widget;
 
+import java.io.Serializable;
+
 import net.numa08.naptimer.R;
 import android.app.DialogFragment;
 import android.os.Bundle;
@@ -9,12 +11,37 @@ import android.view.ViewGroup;
 
 public class RingConfigDialog extends DialogFragment {
 	
+	
 	public static final String IDENTIFIER = RingConfigDialog.class.getSimpleName();
-
-	public static RingConfigDialog newInstance(){
-		return new RingConfigDialog();
+	private static final String EXTRA_LISTENER = RingConfigDialog.class.getPackage().getName() + ".EXTRA_LISTENER";
+	
+	private RingConfigDialogListener mListener;
+	
+	public static interface RingConfigDialogListener extends Serializable{
+		public void onClickOnWithChangeConfig(RingConfigDialog dialog, boolean isRingAlerm , boolean isVibrat);
 	}
 	
+	public static RingConfigDialog newInstance(RingConfigDialogListener listener){
+		final Bundle arg = new Bundle();
+		arg.putSerializable(EXTRA_LISTENER, listener);
+		
+		RingConfigDialog dialog =  new RingConfigDialog();
+		dialog.setArguments(arg);
+		return dialog;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		final Bundle arg = getArguments();
+		mListener = (RingConfigDialogListener) arg.getSerializable(EXTRA_LISTENER);
+		
+		final int style = DialogFragment.STYLE_NO_TITLE;
+		final int theme = android.R.style.Theme_Holo_Light_Dialog_MinWidth;
+		
+		setStyle(style, theme);
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		final View layout = inflater.inflate(R.layout.ring_config_dialog, container, false);
