adfa798a9b13c037c3072f74dde0efc325a6ab43
numa08 <n511287@gmail.com>
2013-04-25 22:54:22 +0900

    メニューボタンを排除

--- src/naptimer/src/net/numa08/naptimer/NappTimer.java
+++ src/naptimer/src/net/numa08/naptimer/NappTimer.java
@@ -2,7 +2,6 @@ package net.numa08.naptimer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 
 public class NappTimer extends Activity {
@@ -11,12 +10,6 @@ public class NappTimer extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_napp_timer);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.napp_timer, menu);
-		return true;
 	}
 	
 	public void clickConfigButton(View view){}
