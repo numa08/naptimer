879cc2d7c553e849774b3aef77adb40f2113c6ac
numa08 <n511287@gmail.com>
2013-04-26 00:31:01 +0900

    ダイアログ実装
    Activityからダイアログを呼び出せる

--- src/naptimer/src/net/numa08/naptimer/NappTimer.java
+++ src/naptimer/src/net/numa08/naptimer/NappTimer.java
@@ -1,10 +1,16 @@
 package net.numa08.naptimer;
 
+import net.numa08.naptimer.widget.RingConfigDialog;
+import net.numa08.naptimer.widget.RingConfigDialog.RingConfigDialogListener;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 
-public class NappTimer extends Activity {
+public class NappTimer extends Activity implements RingConfigDialogListener{
+	
+	private static final long serialVersionUID = -9072959517189184730L;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +18,22 @@ public class NappTimer extends Activity {
 		setContentView(R.layout.activity_napp_timer);
 	}
 	
-	public void clickConfigButton(View view){}
+	public void clickConfigButton(View view){
+		final FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+		final Fragment prevDialog = getFragmentManager().findFragmentByTag(RingConfigDialog.IDENTIFIER);
+		if(prevDialog != null){
+			fragmentTransaction.remove(prevDialog);
+		}
+		fragmentTransaction.addToBackStack(null);
+		
+		final RingConfigDialog dialog = RingConfigDialog.newInstance(this);
+		dialog.show(fragmentTransaction, RingConfigDialog.IDENTIFIER);
+	}
 	
 	public void clickNeruButton(View view){}
+
+	@Override
+	public void onClickOnWithChangeConfig(RingConfigDialog dialog,
+			boolean isRingAlerm, boolean isVibrat) {}
+	
 }
