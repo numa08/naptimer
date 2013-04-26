4fedb4d77c4e4badcf9f233ffc9c9a644eda6d7e
numa08 <n511287@gmail.com>
2013-04-27 06:34:16 +0900

    設定ボタンのイベントをハンドル

--- src/naptimer/src/net/numa08/naptimer/widget/RingConfigDialog.java
+++ src/naptimer/src/net/numa08/naptimer/widget/RingConfigDialog.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 public class RingConfigDialog extends DialogFragment implements OnClickListener{
 	
@@ -18,6 +19,8 @@ public class RingConfigDialog extends DialogFragment implements OnClickListener{
 	private static final String EXTRA_LISTENER = RingConfigDialog.class.getPackage().getName() + ".EXTRA_LISTENER";
 	
 	private RingConfigDialogListener mListener;
+	private Switch mRingAlerSwitch;
+	private Switch mVivrateSwitch;
 	
 	public static interface RingConfigDialogListener extends Serializable{
 		public void onClickOnWithChangeConfig(RingConfigDialog dialog, boolean isRingAlerm , boolean isVibrat);
@@ -48,10 +51,19 @@ public class RingConfigDialog extends DialogFragment implements OnClickListener{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		final View layout = inflater.inflate(R.layout.ring_config_dialog, container, false);
 		layout.findViewById(R.id.dialog_ok_button).setOnClickListener(this);
+		mRingAlerSwitch = (Switch)layout.findViewById(R.id.alertm_switch);
+		mVivrateSwitch = (Switch)layout.findViewById(R.id.vive_switch);
 		return layout;
 	}
 
 	@Override
-	public void onClick(View v) {}
+	public void onClick(View v) {
+		final boolean isRingAlerm = mRingAlerSwitch.isChecked();
+		final boolean isViverate = mVivrateSwitch.isChecked();
+		if(mListener == null){
+			return;
+		}
+		mListener.onClickOnWithChangeConfig(this, isRingAlerm, isViverate);
+	}
 	
 }
